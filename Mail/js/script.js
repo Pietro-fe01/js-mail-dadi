77460b3bdad144f6b8fbb680aba602171cc83355
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-10-25 14:12:08 +0200

    Creato algoritmo per le e-mail - Ho creato un array con 4 mail diverse - Il browser chiede in input la mail dell'utente - Se la mail dell'utente è contenuta nell'array può entrare - altrimenti gli viene stampato un messaggio di errore e la stampa si ricarica

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,3 +1,34 @@
+"use strict";
+
 /*Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.*/
+
+const emailLogged = ["mariorossi@gmail.com", "gianlucaverdi@gmail.com", "oraziomarrone@gmail.com", "classe76@gmail.com"];
+
+let emailInputUser = prompt("Ciao! Accedi con la tua e-mail!");
+
+// PRIMO METODO
+// if (emailLogged.includes(emailInputUser)){
+//     alert("Email riconosciuta, Benvenuto/a!");
+// } else {
+//     alert("Attenzione! La e-mail inserita non è presente all'interno del nostro sistema. La invitiamo a riprovare");
+//     location.reload();
+// }
+
+
+// SECONDO METODO
+const index = 0;
+for (let i = 0; i < emailLogged.length; i++) {
+    if (emailInputUser === emailLogged[i]){
+        alert("Email riconosciuta, Benvenuto/a!");
+        index = 1;
+        break;
+    }
+}
+
+if (index === 0){
+    alert("Attenzione! La e-mail inserita non è presente all'interno del nostro sistema. La invitiamo a riprovare");
+    location.reload();
+}
+
